f43102dec2140a611665e4e88a975abddfe9ccc7
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-07-08 11:48:05 +0700

    feat: add options argument to serve utility

--- packages/utils/src/serve.ts
+++ packages/utils/src/serve.ts
@@ -1,6 +1,12 @@
 import { serve as honoServe } from "@hono/node-server";
 import { type Hono } from "hono";
 
-export function serve(hono: Hono) {
-  return honoServe(hono);
+interface ServeOptions {
+  fetch: Hono["fetch"];
+  port?: number;
+  hostname?: string;
+}
+
+export function serve(options: ServeOptions) {
+  return honoServe(options);
 }
