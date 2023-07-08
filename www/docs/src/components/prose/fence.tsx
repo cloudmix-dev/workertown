70719b85ed3aedcbb259979350b5c72fae59ffaf
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-07-08 16:26:52 +0700

    feat: add copy button to fence component in docs

--- www/docs/src/components/prose/fence.tsx
+++ www/docs/src/components/prose/fence.tsx
@@ -1,6 +1,34 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
 import { Fragment } from "react";
 
+import { cn } from "../../lib/utils";
+import { Button } from "../atoms/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../atoms/tooltip";
+
+function ClipboardIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+      />
+    </svg>
+  );
+}
+
 export function Fence({ children, language }) {
   return (
     <Highlight
@@ -9,22 +37,44 @@ export function Fence({ children, language }) {
       language={language}
       theme={undefined}
     >
-      {({ className, style, tokens, getTokenProps }) => (
-        <pre className={className} style={style}>
-          <code>
-            {tokens.map((line, lineIndex) => (
-              <Fragment key={lineIndex}>
-                {line
-                  .filter((token) => !token.empty)
-                  .map((token, tokenIndex) => (
-                    <span key={tokenIndex} {...getTokenProps({ token })} />
-                  ))}
-                {"\n"}
-              </Fragment>
-            ))}
-          </code>
-        </pre>
-      )}
+      {({ className, style, tokens, getTokenProps }) => {
+        const text = tokens
+          .map((line) => line.map((token) => token.content).join(""))
+          .join("\n");
+        const onCopy = () => navigator.clipboard.writeText(text);
+
+        return (
+          <pre className={cn("relative", className)} style={style}>
+            <code>
+              {tokens.map((line, lineIndex) => (
+                <Fragment key={lineIndex}>
+                  {line
+                    .filter((token) => !token.empty)
+                    .map((token, tokenIndex) => (
+                      <span key={tokenIndex} {...getTokenProps({ token })} />
+                    ))}
+                  {"\n"}
+                </Fragment>
+              ))}
+            </code>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="absolute top-1.5 right-1.5 px-2.5 py-0 dark:bg-zinc-900 dark:text-zinc-50 dark:hover:bg-zinc-900/90"
+                    onClick={onCopy}
+                  >
+                    <ClipboardIcon />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="py-0">
+                  <p>Copy to clipboard</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </pre>
+        );
+      }}
     </Highlight>
   );
 }
