b7d4741eda90633e4500325872671cc4ac36c931
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-07-11 18:12:57 +0700

    docs: add pub-sub to nodejs example

--- examples/nodejs/src/index.ts
+++ examples/nodejs/src/index.ts
@@ -3,6 +3,10 @@ import { SqliteStorageAdapter as FeatureFlagsStorageAdapter } from "@workertown/
 import { kv } from "@workertown/kv";
 import { SqliteStorageAdapter as KvStorageAdapter } from "@workertown/kv/storage/sqlite-storage-adapter";
 import { serve } from "@workertown/node";
+import { pubSub } from "@workertown/pub-sub";
+import { createQueueProcessor } from "@workertown/pub-sub/queue";
+import { SqliteQueueAdapter } from "@workertown/pub-sub/queue/sqlite-queue-adapter";
+import { SqliteStorageAdapter as PubSubStorageAdapter } from "@workertown/pub-sub/storage/sqlite-storage-adapter";
 import { search } from "@workertown/search";
 import { SqliteStorageAdapter as SearchStorageAdapter } from "@workertown/search/storage/sqlite-storage-adapter";
 import { combine } from "@workertown/utils";
@@ -10,42 +14,75 @@ import * as path from "node:path";
 
 const { PORT = "3000", API_KEY = "super_secret_api" } = process.env;
 
-const api = combine(
-  featureFlags({
-    auth: {
-      apiKey: {
-        apiKey: API_KEY,
-      },
+const flagsApi = featureFlags({
+  auth: {
+    apiKey: {
+      apiKey: API_KEY,
     },
-    basePath: "/flags",
-    storage: new FeatureFlagsStorageAdapter({
-      db: path.resolve(__dirname, "../db/flags.sqlite"),
-    }),
+  },
+  basePath: "/flags",
+  storage: new FeatureFlagsStorageAdapter({
+    db: path.resolve(__dirname, "../db/flags.sqlite"),
   }),
-  kv({
-    auth: {
-      apiKey: {
-        apiKey: API_KEY,
-      },
+});
+
+const kvApi = kv({
+  auth: {
+    apiKey: {
+      apiKey: API_KEY,
     },
-    basePath: "/kv",
-    storage: new KvStorageAdapter({
-      db: path.resolve(__dirname, "../db/kv.sqlite"),
-    }),
+  },
+  basePath: "/kv",
+  storage: new KvStorageAdapter({
+    db: path.resolve(__dirname, "../db/kv.sqlite"),
   }),
-  search({
-    auth: {
-      apiKey: {
-        apiKey: API_KEY,
-      },
+});
+
+const pubSubQueueAdapter = new SqliteQueueAdapter({
+  db: path.resolve(__dirname, "../db/pub-sub-queue.sqlite"),
+});
+const pubSubApi = pubSub({
+  auth: {
+    apiKey: {
+      apiKey: API_KEY,
     },
-    basePath: "/search",
-    storage: new SearchStorageAdapter({
-      db: path.resolve(__dirname, "../db/search.sqlite"),
-    }),
-  })
-);
+  },
+  basePath: "/pub-sub",
+  queue: pubSubQueueAdapter,
+  storage: new PubSubStorageAdapter({
+    db: path.resolve(__dirname, "../db/pub-sub.sqlite"),
+  }),
+});
 
-serve(api, { port: parseInt(PORT, 10) });
+const searchApi = search({
+  auth: {
+    apiKey: {
+      apiKey: API_KEY,
+    },
+  },
+  basePath: "/search",
+  storage: new SearchStorageAdapter({
+    db: path.resolve(__dirname, "../db/search.sqlite"),
+  }),
+});
 
-console.log(`Server running at http://localhost:${PORT}`);
+const api = combine(flagsApi, kvApi, pubSubApi, searchApi);
+
+(async function main() {
+  serve(api, { port: parseInt(PORT, 10) });
+
+  console.log(`Server running at http://localhost:${PORT}`);
+
+  const processor = createQueueProcessor({
+    adapter: pubSubQueueAdapter,
+    server: api,
+  });
+
+  await pubSubQueueAdapter.runMigrations();
+
+  setInterval(async () => {
+    await processor.process();
+  }, 1000);
+
+  console.log(`PubSub queue processor running`);
+})();
