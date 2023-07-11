2acbcd02b9a3cd56f876b15dce078ccf199aeb9b
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-07-11 23:11:23 +0700

    feat: open api spec support for search

--- packages/search/src/constants.ts
+++ packages/search/src/constants.ts
@@ -132,3 +132,459 @@ export const DEFAUlT_STOP_WORDS = new Set([
   "should",
   "now",
 ]);
+
+export const OPEN_API_SPEC = {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Workertown Search",
+    license: {
+      name: "MIT",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8787",
+    },
+  ],
+  paths: {
+    "/v1/search/{tenant}": {
+      get: {
+        summary: "Search across a tenant",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "searchTenant",
+        tags: ["Search"],
+        parameters: [
+          {
+            name: "tenant",
+            in: "path",
+            required: true,
+            description: "The tenant to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "term",
+            in: "query",
+            description: "The term to search for",
+            required: true,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "fields",
+            in: "query",
+            description: "Comma separated list of fields to search within",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "tags",
+            in: "query",
+            description: "Comma separated list of tags to filter the search by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "order_by",
+            in: "query",
+            description: "The field to order the results by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "An array of matching items",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/SearchResult",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerError",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/v1/search/{tenant}/{index}": {
+      get: {
+        summary: "Search across a tenant and index",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "searchTenantAndIndex",
+        tags: ["Search"],
+        parameters: [
+          {
+            name: "tenant",
+            in: "path",
+            required: true,
+            description: "The tenant to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "index",
+            in: "path",
+            required: true,
+            description: "The index to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "term",
+            in: "query",
+            description: "The term to search for",
+            required: true,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "fields",
+            in: "query",
+            description: "Comma separated list of fields to search within",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "tags",
+            in: "query",
+            description: "Comma separated list of tags to filter the search by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "order_by",
+            in: "query",
+            description: "The field to order the results by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "An array of matching items",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/SearchResult",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerError",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/v1/suggest/{tenant}": {
+      get: {
+        summary: "Suggest terms for a tenant",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "suggestTenant",
+        tags: ["Suggest"],
+        parameters: [
+          {
+            name: "tenant",
+            in: "path",
+            required: true,
+            description: "The tenant to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "term",
+            in: "query",
+            description: "The term to search for",
+            required: true,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "fields",
+            in: "query",
+            description: "Comma separated list of fields to search within",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "tags",
+            in: "query",
+            description: "Comma separated list of tags to filter the search by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "An array of matching items",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/SuggestResult",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerError",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/v1/suggest/{tenant}/{index}": {
+      get: {
+        summary: "Suggest terms for a tenant",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "suggestTenantAndIndex",
+        tags: ["Suggest"],
+        parameters: [
+          {
+            name: "tenant",
+            in: "path",
+            required: true,
+            description: "The tenant to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "index",
+            in: "path",
+            required: true,
+            description: "The index to search",
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "term",
+            in: "query",
+            description: "The term to search for",
+            required: true,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "fields",
+            in: "query",
+            description: "Comma separated list of fields to search within",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+          {
+            name: "tags",
+            in: "query",
+            description: "Comma separated list of tags to filter the search by",
+            required: false,
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "An array of matching items",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/SuggestResult",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerError",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  } as any,
+  components: {
+    securitySchemes: {
+      BasicAuth: {
+        type: "http",
+        scheme: "basic",
+      },
+      BearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      SearchResult: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "object",
+            properties: {
+              id: {
+                type: "string",
+                example: "a2282e3f-553a-44c2-aa93-590f7b711eba",
+              },
+              item: {
+                type: "object",
+                additionalProperties: true,
+                example: {
+                  id: "1",
+                  tenant: "test",
+                  index: "test",
+                  data: {
+                    title: "Test item 1",
+                    content: "This is some test content",
+                  },
+                  createdAt: "<SOME DATE>",
+                  updatedAt: "<SOME DATE>",
+                },
+              },
+              score: {
+                type: "number",
+                format: "float",
+                example: 0.4315231086776713,
+              },
+              terms: {
+                type: "array",
+                items: {
+                  type: "string",
+                },
+                example: ["test"],
+              },
+              match: {
+                type: "object",
+                additionalProperties: true,
+                example: {
+                  test: ["content"],
+                },
+              },
+            },
+          },
+        },
+      },
+      SuggestResult: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                suggestion: {
+                  type: "string",
+                  example: "test",
+                },
+                terms: {
+                  type: "array",
+                  items: {
+                    type: "string",
+                  },
+                  example: ["test"],
+                },
+                score: {
+                  type: "number",
+                  format: "float",
+                  example: 0.4315231086776713,
+                },
+              },
+            },
+          },
+        },
+      },
+      InternalServerError: {
+        type: "object",
+        required: ["status", "success", "data", "error"],
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 500,
+          },
+          success: {
+            type: "boolean",
+            example: false,
+          },
+          data: {
+            type: "object",
+            example: null,
+          },
+          error: {
+            type: "string",
+            example: "Internal server error",
+          },
+        },
+      },
+    },
+  },
+};
--- packages/search/src/routers/public.ts
+++ packages/search/src/routers/public.ts
@@ -1,19 +1,64 @@
 import { createRouter } from "@workertown/hono";
 
+import { OPEN_API_SPEC } from "../constants";
 import { Context } from "../types";
+
+const CORS_HEADERS = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, OPTIONS",
+  "access-control-allow-private-network": "true",
+};
 
 const router = createRouter<Context>({ public: true });
 
-const openApi = router.get("/open-api.json", (ctx) => {
-  const { prefixes } = ctx.get("config");
+router.options("*", (ctx) =>
+  ctx.text("OK", {
+    headers: CORS_HEADERS,
+  })
+);
 
-  return ctx.jsonT({ prefixes });
+const openApi = router.get("/open-api.json", (ctx) => {
+  const { auth, basePath, prefixes } = ctx.get("config");
+  const url = new URL(ctx.req.url);
+  const searchPath = `${`${basePath}${prefixes.search}`.replace(
+    "//",
+    "/"
+  )}/{tenant}`;
+  const searchIndexPath = `${`${basePath}${prefixes.search}`.replace(
+    "//",
+    "/"
+  )}/{tenant}/{index}`;
+
+  OPEN_API_SPEC.servers[0]!.url = `${url.protocol}//${url.host}`;
+
+  if (!OPEN_API_SPEC.paths[searchPath]) {
+    const search = OPEN_API_SPEC.paths["/v1/search/{tenant}"];
+
+    OPEN_API_SPEC.paths[searchPath] = search;
+
+    delete OPEN_API_SPEC.paths["/v1/search/{tenant}"];
+  }
+
+  if (!OPEN_API_SPEC.paths[searchIndexPath]) {
+    const searchIndex = OPEN_API_SPEC.paths["/v1/search/{tenant}/{index}"];
+
+    OPEN_API_SPEC.paths[searchIndexPath] = searchIndex;
+
+    delete OPEN_API_SPEC.paths["/v1/search/{tenant}/{index}"];
+  }
+
+  return ctx.json(OPEN_API_SPEC, {
+    headers: CORS_HEADERS,
+  });
 });
 
 export type OpenApiRoute = typeof openApi;
 
 const health = router.get("/health", async (ctx) =>
-  ctx.json({ status: 200, success: true, data: "OK" })
+  ctx.json(
+    { status: 200, success: true, data: "OK" },
+    { headers: CORS_HEADERS }
+  )
 );
 
 export type HealthRoute = typeof health;
