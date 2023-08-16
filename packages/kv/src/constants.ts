b4fd5b82dbe4714448afc6362f6eea05bdc26f86
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-08-16 09:58:53 +0100

    feat: add Open API v3 spec for kv

--- packages/kv/src/constants.ts
+++ packages/kv/src/constants.ts
@@ -1,3 +1,420 @@
-export const OPEN_API_SPEC = {
+import { type OpenApiSpec } from "@workertown/internal-open-api";
+
+export const OPEN_API_SPEC: OpenApiSpec = {
   openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Workertown KV",
+    license: {
+      name: "MIT",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8787",
+    },
+  ],
+  paths: {
+    "/v1/kv/{key}": {
+      get: {
+        summary: "Get a value",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "getValue",
+        tags: ["Key/Value"],
+        parameters: [
+          {
+            name: "key",
+            in: "path",
+            required: true,
+            description: "The key for the value",
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "The feature flag",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/GetValueResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+      put: {
+        summary: "Set a value",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "setValue",
+        tags: ["Key/Value"],
+        parameters: [
+          {
+            name: "key",
+            in: "path",
+            required: true,
+            description: "The key for the value",
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        requestBody: {
+          description: "The value to set",
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/SetValueBody",
+              },
+            },
+          },
+        },
+        responses: {
+          "200": {
+            description: "The value to set",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/SetValueResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+      delete: {
+        summary: "Delete a value",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "deleteValue",
+        tags: ["Key/Value"],
+        parameters: [
+          {
+            name: "key",
+            in: "path",
+            required: true,
+            description: "The key for the value",
+            schema: {
+              type: "string",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "The result",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/DeleteValueResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/v1/admin/info": {
+      get: {
+        summary: "Get configuration information",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "adminInfo",
+        tags: ["Admin"],
+        responses: {
+          "200": {
+            description: "The current configuration",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/AdminInfoResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/v1/admin/migrate": {
+      post: {
+        summary: "Run database migrations",
+        security: [{ BasicAuth: [] }, { BearerAuth: [] }],
+        operationId: "adminMigrate",
+        tags: ["Admin"],
+        responses: {
+          "200": {
+            description: "The successfully run migrations",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/AdminMigrateResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/health": {
+      get: {
+        summary: "Get service health",
+        security: [],
+        operationId: "health",
+        tags: ["Public"],
+        responses: {
+          "200": {
+            description: "The service health status",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/PublicHealthResponse",
+                },
+              },
+            },
+          },
+          default: {
+            description: "Unexpected error",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/InternalServerErrorResponse",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    // rome-ignore lint/suspicious/noExplicitAny: We don't care about the type here
+  } as any,
+  components: {
+    securitySchemes: {
+      BasicAuth: {
+        type: "http",
+        scheme: "basic",
+      },
+      BearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      SetValueBody: {
+        properties: {
+          value: {
+            oneOf: [
+              { type: "string" },
+              { type: "number" },
+              { type: "boolean" },
+              { type: "object" },
+              { type: "array" },
+            ],
+            example: "value",
+          },
+        },
+      },
+      GetValueResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            oneOf: [
+              { type: "string" },
+              { type: "number" },
+              { type: "boolean" },
+              { type: "object" },
+              { type: "array" },
+            ],
+            example: "value",
+          },
+        },
+      },
+      SetValueResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            oneOf: [
+              { type: "string" },
+              { type: "number" },
+              { type: "boolean" },
+              { type: "object" },
+              { type: "array" },
+            ],
+            example: "value",
+          },
+        },
+      },
+      DeleteFlagResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "boolean",
+            example: true,
+          },
+        },
+      },
+      AdminInfoResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "object",
+          },
+        },
+      },
+      AdminMigrateResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                migrationName: {
+                  type: "string",
+                  example: "1688823193041_add_initial_tables_and_indexes",
+                },
+                direction: {
+                  type: "string",
+                  example: "Up",
+                },
+                status: {
+                  type: "string",
+                  example: "Success",
+                },
+              },
+            },
+          },
+        },
+      },
+      PublicHealthResponse: {
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 200,
+          },
+          success: {
+            type: "boolean",
+            example: true,
+          },
+          data: {
+            type: "string",
+            example: "OK",
+          },
+        },
+      },
+      InternalServerErrorResponse: {
+        type: "object",
+        required: ["status", "success", "data", "error"],
+        properties: {
+          status: {
+            type: "integer",
+            format: "int32",
+            example: 500,
+          },
+          success: {
+            type: "boolean",
+            example: false,
+          },
+          data: {
+            type: "object",
+            example: null,
+          },
+          error: {
+            type: "string",
+            example: "Internal server error",
+          },
+        },
+      },
+    },
+  },
 };
--- packages/kv/src/routers/public.ts
+++ packages/kv/src/routers/public.ts
@@ -1,12 +1,42 @@
 import { createRouter } from "@workertown/internal-hono";
+import { generateOpenApiSpec } from "@workertown/internal-open-api";
 
 import { OPEN_API_SPEC } from "../constants.js";
 import { type Context } from "../types.js";
 
+const CORS_HEADERS = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, OPTIONS",
+  "access-control-allow-private-network": "true",
+};
+
 const router = createRouter<Context>({ public: true });
 
+router.options("/open-api.json", (ctx) =>
+  ctx.text("OK", {
+    headers: CORS_HEADERS,
+  }),
+);
+
 router.get("/open-api.json", (ctx) => {
-  return ctx.json(OPEN_API_SPEC);
+  const { basePath = "/", endpoints } = ctx.get("config");
+  const url = new URL(ctx.req.url);
+  const replacementPaths: Record<string, string | false> = {
+    "/v1/kv": endpoints.v1.kv,
+    "/v1/admin": endpoints.v1.admin,
+    "/health": endpoints.public
+      ? `${endpoints.public === "/" ? "" : endpoints.public}/health`
+      : false,
+  };
+  const spec = generateOpenApiSpec(OPEN_API_SPEC, {
+    basePath,
+    urls: [`${url.protocol}//${url.host}`],
+    endpoints: replacementPaths,
+  });
+
+  return ctx.json(spec, {
+    headers: CORS_HEADERS,
+  });
 });
 
 router.get("/health", async (ctx) =>
